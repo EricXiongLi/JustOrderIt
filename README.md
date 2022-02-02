bbd78701cef93a64871f3461cbb22d03abc6b1df
EricXiongLi <xiong.li@uzh.ch>
2022-02-02 15:30:15 +0100

    simple UI achieved

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is an online order application built with React.js
+
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Just Order It</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
